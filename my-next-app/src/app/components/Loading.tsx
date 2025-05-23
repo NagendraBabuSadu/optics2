065b6f8f4b7e9e3d8f30ad6c34da09c94ec06d12
nagendrababusadu <nagendrababusadu@outlook.com>
2025-05-23 15:25:46 +0530

    added a few more changes

---
+++ my-next-app/src/app/components/Loading.tsx
@@ -0,0 +1,11 @@
+import "./Loading.css"; 
+
+
+export default function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <p className="loading-text">Loading...</p>
+    </div>
+  );
+}
--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,18 +1,23 @@
-import ContactUs from "./components/ContactUs";
-import Content from "./components/Content";
-import GridImages from "./components/GridImages";
-import ImageCarousel from "./components/ImageCarousel";
+import { lazy, Suspense } from "react";
+import Loading from "./components/Loading";
+
+const ContactUs = lazy(() => import("./components/ContactUs"));
+const Content = lazy(() => import("./components/Content"));
+const GridImages = lazy(() => import("./components/GridImages"));
+const ImageCarousel = lazy(() => import("./components/ImageCarousel"));
 
 export default function Home() {
   return (
     <div className="grid min-h-screen">
       <main className="flex flex-col gap-[12px] items-center justify-center">
-        <ImageCarousel />
-        <Content />
-        <GridImages />
-        <ContactUs />
+        <Suspense fallback={<Loading />}>
+          <ImageCarousel />
+          <Content />
+          <GridImages />
+          <ContactUs />
+        </Suspense>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-10  bg-slate-600 p-4" >
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-10  bg-slate-600 p-4">
         <div className="container mx-auto text-center text-sm">
           &copy; 2025&nbsp;
           <span className="font-semibold">Sai Optical Express. </span>
