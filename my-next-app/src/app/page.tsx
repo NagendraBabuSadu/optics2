ed3c1ea5aa8f7902925fd8a81f99ae017ffddefe
nagendrababusadu <nagendrababusadu@outlook.com>
2025-05-23 14:09:12 +0530

    changes done

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,14 +1,17 @@
-import Navbar from "./components/Navbar";
+import Content from "./components/Content";
+import ImageCarousel from "./components/ImageCarousel";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Navbar />
+    <div className="grid min-h-screen">
+      <main className="flex flex-col gap-[12px] items-center justify-center">
+        <ImageCarousel />
+        <Content />
+        
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <div className="container mx-auto text-center text-sm">
-          &copy; 2025
+          &copy; 2025&nbsp;
           <span className="font-semibold">Sai Optical Express. </span>
           Designed with ❤️ in India.
         </div>
